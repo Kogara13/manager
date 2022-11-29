604d36882f20ffd982963259066a5d93a11bb176
Kogara13 <kieranogara@gmail.com>
2022-11-29 00:17:13 +0000

    fixed Account query in addMenu()

--- main.py
+++ main.py
@@ -12,12 +12,49 @@ def main():
 
     check = database.passwordCheck()
     if (check):
-        menu.mainMenu()
+       mainMenu()
     else:
         print("error")
     print("\nGoodbye\n")
     sleep(1) 
     return
 
+"""
+def mainMenu():
+    os.system("clear")
+    menu.generateTitle("Main Menu")
+    print(" 1. Add Password\n
+            2. Delete Password\n
+            3. Edit Password\n
+            4. Generate Password\n
+            Q. Quit")
+    print("-----------------------------------------")
+    while(True):
+        selection = input("Choose an option: ")
+        if selection == '1':
+            menu.addMenu()
+        elif selection == '2':
+            menu.deleteMenu()
+        elif selection == '3':
+            menu.editMenu()
+        elif selection == '4':
+            menu.generateMenu()
+        elif (selection == 'Q' or selection == 'q'):
+            os.system('clear')
+            confirmQuit = input("Are you sure you want to quit? ")
+            if (confirmQuit == 'N' or confirmQuit == 'n'):
+                continue
+            elif (confirmQuit == 'Y' or confirmQuit == 'y'):
+                return
+            else:
+                print("Invalid entry. Returning to Main Menu...")
+                sleep(1)
+                continue
+"""
+
 if __name__ == "__main__":
     main()
+
+
+
+
