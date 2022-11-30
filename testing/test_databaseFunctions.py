1c7b8d8245e29d775b3d91535372049f53ed14ac
Kogara13 <kieranogara@gmail.com>
2022-11-30 05:59:34 +0000

    Added comments

--- testing/test_databaseFunctions.py
+++ testing/test_databaseFunctions.py
@@ -4,8 +4,8 @@ from databaseFunctions import deleteSelectedPassword
 
 class TestDatabaseInput(unittest.TestCase):
     def test_add(self):
-        #Returns what was inputted into the database as a tuple. Test to see that the correct strings were inputted
            
+        # Test to see that data is successfully added to the database. Returns the number of rows with provided Account name in a tuple. 
         self.assertEqual(addPassword('test0', 'pass0', 'pass0'), ('test0', 'pass0'))
         self.assertEqual(addPassword('test1', 'pass1', 'pass1'), ('test1', 'pass1'))
         self.assertEqual(addPassword('test2', 'pass2', 'pass2'), ('test2', 'pass2'))
@@ -15,14 +15,16 @@ class TestDatabaseInput(unittest.TestCase):
         self.assertEqual(addPassword('test6', 'pass6', 'pass6'), ('test6', 'pass6'))
         self.assertEqual(addPassword('test7', 'pass7', 'pass7'), ('test7', 'pass7'))
         self.assertEqual(addPassword('test8', 'pass8', 'pass8'), ('test8', 'pass8'))
-        self.assertEqual(addPassword('test9', 'pass9', 'pass9'), ('test9', 'pass9'))
+        self.assertEqual(addPassword('test9', 'pass9', 'pass9'), ('test9', 'pass9')) 
            
+        #Test to see that data is successfully removed from the database. Returns the number of rows with provided Account name in a tuple.
         self.assertEqual(deleteSelectedPassword('test0', 'Y'), (0,))
         self.assertEqual(deleteSelectedPassword('test1', 'Y'), (0,))
         self.assertEqual(deleteSelectedPassword('test2', 'Y'), (0,))
         self.assertEqual(deleteSelectedPassword('test3', 'Y'), (0,))
         self.assertEqual(deleteSelectedPassword('test4', 'Y'), (0,))
 
+        #Test to see that deletion from database can be suspended. Returns the number of rows with provided Account name in a tuple.
         self.assertEqual(deleteSelectedPassword('test5', 'N'), (1,))
         self.assertEqual(deleteSelectedPassword('test6', 'n'), (1,))
         self.assertEqual(deleteSelectedPassword('test7', 'N'), (1,))
